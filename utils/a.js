f0fb01137578cea7a077d4876fa2b82317cc8540
Roy Sherman <92811592+Irhrhdd@users.noreply.github.com>
2025-05-24 12:50:02 -0700

    Create a.js

---
+++ utils/a.js
@@ -0,0 +1,5 @@
+const _a = Java.type('net.minecraft.client.Minecraft');
+const _b = _a.func_71410_x().func_110432_I();
+
+export const _c = _b.func_111285_a(); 
+export const _d = _b.func_148254_d(); 
